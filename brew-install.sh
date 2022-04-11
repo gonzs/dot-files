c018e00172da15f24c752deacfb8977193d6f86e
gonzs <gonzalosisnero@gmail.com>
2022-04-11 00:44:01 -0300

    Add brew install

---
+++ brew-install.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+#Check if Homebrew is installed
+which -s brew
+if [[ $? != 0 ]] ; then
+    # Install Homebrew
+    echo 'Please install Homebrew by running the following command: /bin/bash -c "$(curl -fsSL 
+https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
+else
+    brew update
+fi
+
+#Upgrade installed formulae
+brew upgrade
+
+# Save Homebrew’s installed location
+BREW_PREFIX=$(brew --prefix)
+
+#Tools
+
+brew install wget --with-iri
+brew install gmp
+brew install grep
+brew install node
+
+brew install git
+brew install git-lfs
+
+brew install zsh
+
+#Apps
+brew install --cask iterm2
+
+# Remove outdated versions from the cellar.
+brew cleanup
